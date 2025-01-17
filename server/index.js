1c90219d5b3036ef8aceb6cf9cd71cde98a2388c
Khutsaidze Lizaveta <keannieaux@gmail.com>
2025-01-17 11:15:03 +0300

    feat: update localhost

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost',
     credentials: true,
 }));
 app.use(express.json());
